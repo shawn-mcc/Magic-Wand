11e931b16e8ef9f364ab254e420fb1c5da029fa4
Shawn McCausland <shawnmcc1999@gmail.com>
2022-10-17 15:15:18 -0400

    Add files via upload

---
+++ Shawns_Magic_Wand.ps1
@@ -0,0 +1,341 @@
+$Wizard =@"  
+            
+
+.--''''''''''''''''''''''''-vBQl'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+y=oy_.''''''''''''''''''''.,v9\;'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+@Qkv5v-'''''''''''''-.'..^9BV..n^,''''''.''''''''''''''''''''''''''''''''''''''''''''''''''''''
+@B8BeaQL.'''''''''.;eBQY-^s<..;DQm-'.Ys8,.''.''''''''''''''''''''''''''''''''''''''''''''''''''
+g@09Q85QZ='''''''''.rI^r,,uzd..=_''-)Q@BY._D0*'''''''''''''''''''''''''''''''''''''''''''''''''
+r@BRODBQVVr.'''''''''''''=@@E;.'''''..]='',@@|'''''''''''''''''''''''''''''''''''''''''''''''''
+=G@8ONNgBZ=..'''''''''''';xO_''''''''''''',@@|'''''''''''''''''''''''''''''''''''''''''''''''''
+0r@B6OONDBBr.''''''''''''''''''''''''''''',@@l'''''''''''''''''''''''''''''''''''''''''''''''''
+8ZK@0NONNNg@0,..'''''''''''''''''''''''''',@@V'''''''''''''''''''''''''''''''''''''''''''''''''
+,0^0B9OOOONDBB*...''''''''''''''''''''''''=@@o..'''''''''''''''''''''''''''''''''''''''''''''''
+.-,=@QNOOOOONg@9,=*''''''''''''''''''''',=KBdIVn=''''''''''''''''''''''''''''''''''''''''''''''
+'''.p@0NNONE0g0BRL9.'''''''''''''''''''_RE0YL^^(g;..'''''''''''''''''''''''''''''''''''''''''''
+''''<@B6g0Mjl)^*Z=Z,''''''''''''''''''..^)Be]\^<ZP.''''''''''''''''''''''''''''''''''''''''''''
+''...Z@9urcuuvcj3Y*,'''''''''''''''.,y3h)m8EKnVxBx..'''''''''''''''''''''''''''''''''''''''''''
+.>r_rD)Ly^lR|xyByq.''''''''''''''._\YKQ@gxvGMEBBp,v.'''''''''''''''''''''''''''''''''''''''''''
+,8..BrcV^^*YlY^^)Gyr].'''''''''.,**yD9QB^<^lB@p*kn.''''''''''''''''''''''''''''''''''''''''''''
+x8.=B\cyllur=>KapZL\*.'''''''''.=rdENNg@n*wBBvkZr.'''''''...'''''''..'.-,-.>^...,.'._=-''''''''
+p^_Md-,._=.._xV<;U-.=,..'.....-v686NONDBQBBBB^d-''''''''.xy*,,^xyXtzwwkyknL)x\xVwypZdZUIY,'..=-
+,VB@@Vn-_,,'',..^D9kKZRKYr=_xOBQ6N55OO6B@Q0QB==.''''''''.q_NDDdV^===========vu5Ow)======*XpXv=(
+^xY8EwByQfY..x;,BBB69D08DadQB06Odk==Y5O8@Q0QB,.'''''''''.\,@r,*laPml*==========;xLYv;======*V3U
+l3QQB80@Q@ZP=|v)BB8OONU^vn6ONNOOMrl*yNNE@B0B9.'''''''''''.^@(,=vr=,;YVVycr=========*LYL|=======
+QgNNNON90@BBQ@)=gBgO6OI>^yNNNiPOMONNONN6BBgBG.'''''''''''.lB=,BBQQ89ov=,^lyVl^========;v]lVM080
+9OOOOOOODBEQ@QB<QBBgQ6ddNOOMx._^VdOOOOOOQBg@o.'''''''''''.p0,)BENZGZgQBEmx=,<YYL*==(YVllL\wPxr;
+ONOOOOOOOOO606BBBREBDdgD9NOO=lkYZONOONN6D@B@r.'''''''''''.BK,zQ3)xlYYwN6DQBQ6k;,rBZx*=====vpYjU
+OOOOOOOOOOOOOOg@0OOB6_<YwH6gBBBBBBBBBBBgbM3y>\'''''''''''.@c,M8vdNOOOOOOOOONRgQvv@n===^\cwjPu)=
+OOOOOOOOOOOOONNRNOOQ@v..''..--------...-;LwaV_'''''''''''.@Y,RgbNOOOOOOOOOOONN8HY@r==gQMr======
+omN9NNONOOOOOOONOOOR@Q-.'''''''''''''''''''''''''''''''''.@l,d8NOOOOOOOOOOOOON8ZV@;==8^)VIaGjL;
+<h9BQNOOOOOOOOOOOONOQ@c.'''''''''''''''''''''''''''''''.v_@L,VQ6OOOOOOOOOOOOONQpsB===B*=;==<nHE
+NOg@B6ONOOOOOMc,rxqORB@_'''''''''''''''''''''''''''..,;|b,By,,M006ONONOOOOOON6Bld6===BL=)nIhl<*
+ONB@@ROOOOOOOZ^.<vZOOQ@\'''''''''''''''''''''''''''-=rYwlmB0zi;=iH00NOOOOOOOORB=8H===Bu=rYlr;==
+9QBl@DONOOOOOOM3NNOOOE@Z..''''''''''''''''''.'.=rlfdD0plxLytmOQB8PulzpgD6OOON89,gU===BV===;vL;=
+BB;=@0OOOOOOOOOOOOOOO6BB=.r,''''''''''''.=.vGN00N3nlcmGjoIGeZpyVuKRBg3YYXOQQgBk,OK===BV===rx(cr
+B*.v@ROOZddOOOOOOOOOOOB@l.Vr''''''''''''w);H|^===55IYiH9MIcK8MUzIhyvlMBQqx;rvx=,Mf===Bk===\(,kl
+L..v@9OOx=,yNOOOOOOOOOg@D.I|'''''''''''.K0(p0P|===xymEenHD0Nux9QOmnyPmyr]K0DV=,,OV===BM====;x(=
+-..yBNZx;.=lZOOOOOOOOO6B@;rM,'''''''''''.|Xsll0gs*===rVOeY(dBg8y]pdfjVlPmc^*yOdlQr;rxBp========
+...RQOOOoYONNOOOOOOOOOOEBQ-,M-''''''''''''''=LYLc88a)===^YmZIvlOgQEVjZMUXxGqm;;8MyY*=@s=======*
+;.;BgNOOOOONNOOOOOOOpOOOD@y.=x.'''''''''''''''.^=.rsB0o*====\I9BzYUPyVd3P3y6Q;>B-'''.pqV\=====i
+^.l@ROOOOOOOOOOOOONd,^;kNQ@x.;_'''''''''''''''''''..-\eQRz*====rymGzQD5zvl@8*=ob.'''''.=YnVkv;n
+..DB6OOOOOOOOOOOOOGc._,vGNBB,.'''''''''''''''''''''''''.=YM0dV*====Y3mkVQB]==\B;''''''''''.,VbM
+.^@8OOOOOOOOOOOOOONh;adUPORBZ..''''''''''''''''''''''''''''-<VZZPx;==;xhw;===hB.''''''''''''_^V
+;BQNOOOOOOOOOOOOOOONNONNNOO9Bv.''''''''''''''''''''''''''''''''',lHdl^=======Qj._''''''''''''''
+0B9OOOOOOOOOOOOOOOOOOOOOOOOO80,'''''''''''''''''''''''''''''''''''.,wM3Z5wyzMV,K^''''''''''''''
+@gNOOOOOOOOOOOOOOOOOOOOOOOOONB8.''''''''''''''''''''''''''''''''''''''.,vK9EM3Zx.''''''''''''''
+gN6D8BBBBBQ09NNOOOOOOONDgQBBQB@,.'''''''''''''''''''''''''''''''''''''''''.--''''''''''''''''''
+QB@@@@@@@@@@gQQg000gQBB@@@@@@p)(=''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Q@@@@@@@@Bm,'.,^\]v==lOQB@@@Q)w='''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+idQBBBg3\,'''''''''''''..,,,=lY.'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+ _____ _                          _      ___  ___            _        _    _                 _ 
+/  ___| |                        ( )     |  \/  |           (_)      | |  | |               | |
+\ ``--.| |__   __ ___      ___ __ |/ ___  | .  . | __ _  __ _ _  ___  | |  | | __ _ _ __   __| |
+ ``--. \ '_ \ / _`` \ \ /\ / / '_ \  / __| | |\/| |/ _`` |/ _`` | |/ __| | |/\| |/ _`` | '_ \ / _`` |
+/\__/ / | | | (_| |\ V  V /| | | | \__ \ | |  | | (_| | (_| | | (__  \  /\  / (_| | | | | (_| |
+\____/|_| |_|\__,_| \_/\_/ |_| |_| |___/ \_|  |_/\__,_|\__, |_|\___|  \/  \/ \__,_|_| |_|\__,_|
+                                                        __/ |                                  
+                                                       |___/                                   
+"@
+
+Write-Host $Wizard -ForegroundColor Cyan
+
+###Location Check##
+
+Write-Host "Performing Location Check..." -ForegroundColor Yellow
+
+
+If(!($PSCommandPath -eq "$env:USERPROFILE\Desktop\Shawns_Magic_wand.ps1")){
+    [console]::beep(1000,900)
+	[console]::beep(1000,900)
+    Write-Host "Friend, the directions were simple. This only works if you put me on the desktop.Press ENTER to exit, put me in the right spot, and lets try this again" -ForegroundColor Yellow -BackgroundColor Red
+    Read-Host
+    Exit
+}Else{
+    Write-Host "Location on Desktop Verified" -ForegroundColor Green
+}
+
+##Spell Selection##
+#1.NPS
+#2.Tune Up
+
+Write-Host "Thank you for using choosing my Magic Wand. Please select a spell from the options below: " -ForegroundColor Cyan
+Write-Host "1 - NPS " -ForegroundColor Cyan
+Write-Host "2- Tune Up " -ForegroundColor Cyan
+$SpellSelection = Read-Host "Enter the number for the spell you want the wizard to cast "
+
+Switch($SpellSelection){
+
+1 {
+###NPS TABLE OF CONTENTS###
+#1.Install Google Chrome
+#2.Install Adobe Reader
+#3.Windows Updates
+#4.Application Updates
+#5.Create a Restore Point
+Write-Host "You have chosen to cast NPS. The following actions will be performed: " -ForegroundColor Cyan
+Write-Host "- Install Google Chrome" -ForegroundColor Cyan
+Write-Host "- Install Adobe Reader" -ForegroundColor Cyan
+Write-Host "- Windows Updates" -ForegroundColor Cyan
+Write-Host "- Application Updates" -ForegroundColor Cyan
+$confirm = Read-Host "Are you sure you wish to proceed? (y/n)"
+
+If (!($confirm -eq "y")){
+    Write-Host "The magic has fizzled out. Press any key to exit and try again (cancelled)" -ForegroundColor Red
+    Read-Host
+    Exit
+}Else{
+    Write-Host "NPS confirmed. The wizard is beginning his ritual" -ForegroundColor Green
+}
+
+#1.Install Google Chrome
+Write-Host "Installing Google Chrome" -ForegroundColor Cyan
+$Path = $env:TEMP; $Installer = "chrome_installer.exe"
+Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer
+Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
+Remove-Item $Path\$Installer
+Write-Host "Google Chrome Installed" -ForegroundColor Green
+
+#2.Install Adobe Reader
+Write-Host "Installing Adobe Reader" -ForegroundColor Cyan
+$LocalTempDir = $env:TEMP
+$AdobeReaderInstaller = "AdobeReaderInstaller.exe"
+(new-object    System.Net.WebClient).DownloadFile('https://admdownload.adobe.com/bin/live/readerdc64_uk_xa_crd_install.exe', "$LocalTempDir\$AdobeReaderInstaller"); & "$LocalTempDir\$AdobeReaderInstaller" /silent /install
+$Process2Monitor =  "AdobeReaderInstaller"
+Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name
+If ($ProcessesFound){
+"Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2
+}Else{ 
+rm "$LocalTempDir\$AdobeReaderInstaller" -ErrorAction SilentlyContinue -Verbose 
+} 
+} Until (!$ProcessesFound)
+Write-Host "Adobe Reader Installed" -ForegroundColor Green
+
+#3.Windows Updates
+Write-Host "Preparing Windows Updates" -ForegroundColor Cyan
+Install-PackageProvider -Name NuGet -Force
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Install-Module -Name PSWindowsUpdate -Force
+Import-Module PSWindowsUpdate
+Get-WindowsUpdate 
+Install-WindowsUpdate -AcceptAll -IgnoreReboot
+Write-Host "Windows Updates completed" -ForegroundColor Green
+
+#4.Application Updates
+Write-Host "Updating Applicable Apps" -ForegroundColor Cyan
+Write-Output y|winget upgrade -h --all
+Write-Host "App Updates completed" -ForegroundColor Green
+
+#5.Create Restore Point
+Write-Host "Creating System Restore Point" -ForegroundColor Cyan
+Checkpoint-Computer -Description "Maintanence" -RestorePointType "MODIFY_SETTINGS"
+Write-Host "System Restore Point created" -ForegroundColor Green
+
+Write-Host "Basic NPS complete" -ForegroundColor Green
+Break
+}#1 (NPS)
+2 {
+###TUNE UP TABLE OF CONTENTS###
+#1.Create A Restore Point
+#2.Chkdsk On Next Boot
+#3.DISM
+#4.SFC
+#5.Delete User Temp files
+#6.Delete Windows Temp files
+#7.Disk Cleanup
+#8.Optimize Drive
+#9.Windows Updates
+#10.App Updates
+
+Write-Host "You have chosen to cast NPS. The following actions will be performed: " -ForegroundColor Cyan
+Write-Host "- Create a Restore Point" -ForegroundColor Cyan
+Write-Host "- chkdsk enabled on next reboot" -ForegroundColor Cyan
+Write-Host "- DISM" -ForegroundColor Cyan
+Write-Host "- SFC" -ForegroundColor Cyan
+Write-Host "- Delete User Tmp Files" -ForegroundColor Cyan
+Write-Host "- Delete Operating System Tmp Files" -ForegroundColor Cyan
+Write-Host "- Disk Cleanup" -ForegroundColor Cyan
+Write-Host "- Drive Optimization" -ForegroundColor Cyan
+Write-Host "- Windows Updates" -ForegroundColor Cyan
+Write-Host "- App Updates" -ForegroundColor Cyan
+$confirm = Read-Host "Are you sure you wish to proceed? (y/n)"
+
+If (!($confirm -eq "y")){
+    Write-Host "The magic has fizzled out. Press any key to exit and try again (cancelled)" -ForegroundColor Red
+    Read-Host
+    Exit
+}Else{
+    Write-Host "Tune Up confirmed. The wizard is beginning his ritual" -ForegroundColor Green
+}
+
+#1.Creating a Restore Point 
+    Write-Host "Creating System Restore Point" -ForegroundColor Yellow
+    Checkpoint-Computer -Description "Maintanence" -RestorePointType "MODIFY_SETTINGS"
+   	Write-Host "Creating System Restore Point completed" -ForegroundColor Green
+
+#2.Chkdsk On Next Boot
+    Write-Host "Enabling chkdsk on next boot" -ForegroundColor Yellow
+    Write-Output y|chkdsk c: /x /f /r
+    Write-Host "chkdsk will be completed next reboot" -ForegroundColor Green
+
+#3.DISM
+	Write-Host "Starting DISM" -ForegroundColor Yellow
+    Write-Host "WARNING: DISM and SFC can both take a long time to run. Magic Wand is still running. Ctrl + Break should break the script if it appears to be unresponsive for a long (<6 hr) time" -ForegroundColor Red -BackgroundColor Yellow
+    Start-Process -FilePath "dism.exe" -ArgumentList '/online /cleanup-image /startcomponentcleanup' -Wait -NoNewWindow
+    Write-Host "DISM 50% complete" -ForegroundColor DarkYellow
+    Start-Process -FilePath "dism.exe" -ArgumentList '/online /cleanup-image /restorehealth' -Wait -NoNewWindow
+    Write-Host "DISM completed" -ForegroundColor Green
+
+#4.SFC
+    Write-Host "Starting SFC" -ForegroundColor Yellow
+    Start-Process -FilePath "sfc.exe" -ArgumentList '/scannow' -Wait -NoNewWindow 
+   	Write-Host "SFC completed" -ForegroundColor Green
+
+#5.Delete User Temp files   
+	$temp = get-ChildItem "env:\TEMP"   
+	$temp2 = $temp.Value 
+    Write-Host "Deleting User Temp files" -ForegroundColor Yellow    
+    Remove-Item -Recurse  "$temp2\*" -Force -ErrorAction SilentlyContinue
+    Write-Host "Temporary User files deleted sucessfully" -ForegroundColor Green 
+           
+#6.Delete Windows Temp files    
+	$WinTemp = "c:\Windows\Temp\*" 
+    Write-Host "Deleting Windows Temp files" -ForegroundColor Yellow   
+    Remove-Item -Recurse $WinTemp -Force -ErrorAction SilentlyContinue
+    Write-Host "Temporary Windows files deleted sucessfully" -ForegroundColor Green 
+      
+#7.Disk Cleanup    
+    Write-Host "Starting Disk Cleanup" -ForegroundColor Yellow   
+    cleanmgr /sagerun:1 | out-Null  
+    Start-Sleep 2            
+    Write-Host "Disk Cleanup completed" -ForegroundColor Green
+
+#8.Optimize Drive
+	Write-Host "Starting Drive Optimization" -ForegroundColor Yellow
+	Optimize-Volume -DriveLetter C 
+	Write-Host "Drive Optimization Complete" -ForegroundColor Green
+
+#9.Windows Updates
+	Write-Host "Preparing Windows Updates" -ForegroundColor Yellow
+   	Install-PackageProvider -Name NuGet -Force
+	Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+    Install-Module -Name PSWindowsUpdate -Force
+   	Import-Module PSWindowsUpdate
+	Get-WindowsUpdate 
+    Install-WindowsUpdate -AcceptAll -IgnoreReboot
+	Write-Host "Windows Updates completed" -ForegroundColor Green
+
+#10.App Updates
+	Write-Host "Updating Applicable Apps" -ForegroundColor Yellow
+	Write-Output y|winget upgrade -h --all
+	Write-Host "App Updates completed" -ForegroundColor Green
+
+
+Break
+}#2 (Tune Up)
+Default {
+Write-Host "A number my child. All you had to do was pick a number. Now I'm broken. Press any key to exit and try again. And let's get it right this time" -ForegroundColor Yellow -BackgroundColor Red
+Read-Host
+Exit
+}
+}#Switch
+
+#ENDGAME TABLE OF CONTENTS##
+#1.Clear Recent Items
+#2.Remove WindowsPowerShell Folder
+#3.Empty Recycle Bin
+#4.Create Delete (Dispel Magic) Script
+#5.Restart
+
+#1.Clear Recent Items
+	Write-Host "Clearing Recent Items" -ForegroundColor Yellow
+	Get-ChildItem $env:appdata\Microsoft\Windows\Recent\* -Recurse | Remove-Item -Force -Recurse
+	Write-Host "Clearing Recent Items completed" -ForegroundColor Green
+#2.Remove WindowsPowerShell Folder
+	Write-Host "Removing WindowsPowerShell folder" -ForegroundColor Yellow
+	if (Test-Path "$env:userprofile\Documents\WindowsPowerShell") {
+  	Remove-Item $env:userprofile\Documents\WindowsPowerShell -Recurse
+	}
+	Write-Host "Removing WindowsPowerShell folder completed" -ForegroundColor Green
+
+#3.Empty Recycle Bin
+	Write-Host "Emptying Recycle Bin" -ForegroundColor Yellow
+	Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+	Write-Host "Emptying Recycle Bin completed" -ForegroundColor Green
+
+#4.Create Delete (Dispel Magic) Script
+    $StartupPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+
+    $DelScript = "Dispel_Magic.ps1"
+
+    $DelBatch = "Dispel_Magic.bat"
+
+    $BatchContents = @"
+    @echo OFF
+    PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""$env:USERPROFILE\Desktop\Dispel_Magic.ps1""' -Verb RunAs}"
+    
+
+"@
+    $ScriptContents = @"
+
+    Write-Host "Magic Wand sucessfully ran. Preparing Dispel Magic to Delete" -ForegroundColor Yellow -BackgroundColor DarkGreen
+
+    `$WandPath = "`$env:USERPROFILE\Desktop\Shawns_Magic_Wand.ps1"
+
+    `$HasError = `$True
+
+    If (Test-Path `$WandPath){
+        Write-Host "Found Wand - Deleting" -ForegroundColor Yellow
+        Remove-Item -Path `$WandPath
+        If (!(Test-Path `$WandPath)){
+            Write-Host "Magic Wand Sucessfully Deleted" -ForegroundColor Green
+            `$HasError = `$False
+        }Else{
+            Write-Host "FATAL ERROR: Magic Wand was unable to be automatically uninstalled. Please manually dispel." -ForegroundColor Red -BackgroundColor Yellow
+            Read-Host
+        }
+    }Else{
+        Write-Host "FATAL ERROR: Magic Wand was unable to be automatically uninstalled because it was not able to be located on the Desktop. Please double check and manually dispel." -ForegroundColor Red -BackgroundColor Yellow
+        Read-Host
+    }
+
+    If ((Test-Path `$PSCommandPath) -and (`$HasError -eq `$False)){
+        Write-Host "Thank you for using Shawn's Magic Wand. Please press ENTER and Dispel Magic will dispel itself" -ForegroundColor Cyan
+        Read-Host
+        Remove-Item -Path `$PSCommandPath
+        Exit
+
+    }
+"@
+
+If (!(Test-Path $StartupPath\Dispel_Magic.bat)){
+    New-Item -Path $env:USERPROFILE\Desktop -name $DelScript -Type "file" -Value $ScriptContents
+    New-Item -Path $StartupPath -name $DelBatch -Type "file" -Value $BatchContents
+    Write-Host "Created Dispel Magic"
+}Else{
+    Write-Host "ERROR: Dispel Magic already exists apparently. Something broke. Please Manually Investigate Desktop and Startup folder" -ForegroundColor Red -BackgroundColor Yellow
+}
+
+#5.Restart
+    shutdown /r /t 0
